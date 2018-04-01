23eed5e7f021db6209e0aae959f3b050c2e2079c
JohnnysMartins <johnnys.adson@gmail.com>
2018-04-01 18:25:01 -0300

    removing dead code and adjusting the indentation.

--- lib/hell_triangle.rb
+++ lib/hell_triangle.rb
@@ -1,57 +1,56 @@
 class HellTriangle
-    def initialize(triangle)
-        @triangle = triangle
+  def initialize(triangle)
+    @triangle = triangle
+  end
+
+  def total_sum
+    if is_valid_triangle?
+      return total_calculates
+    end
+    return nil
+  end
+
+  private
+
+  def total_calculates
+    while @triangle.length > 1
+      last_row = @triangle.pop
+      last_but_one_row = @triangle.pop
+      value = last_but_one_row.map.with_index { |number, index|
+        number + [last_row[index], last_row[index + 1]].max
+      }
+      @triangle.push(value)
+    end
+    return @triangle[0][0]
+  end
+
+  def is_valid_triangle?
+    if !(@triangle.kind_of? Array) || @triangle.empty?
+      return false
     end
 
-    def total_sum
-        if is_valid_triangle?
-            return total_calculates
-        end
-        return nil
+    return is_valid_triangle_row?
+  end
+
+  def is_valid_triangle_row?
+    (0...@triangle.length).each do |i|
+      row = @triangle[i]
+
+      if !(row.kind_of? Array) && row.length == (i + 1)
+        return false
+      end
+
+      if !is_item_valid?(row)
+        return false
+      end
     end
-    private
-    def total_calculates
-        while @triangle.length > 1 do
-            last_row = @triangle.pop
-            last_but_one_row = @triangle.pop
-            value = last_but_one_row.map.with_index { |number,index| 
-                number + [last_row[index], last_row[index + 1]].max
-            }
-            @triangle.push(value)
-        end
-        # @triangle[0][0] = 15
-        return @triangle[0][0]
+    return true
+  end
+
+  def is_item_valid?(row)
+    row.each do |item|
+      return false unless item.kind_of? Integer
     end
-
-    def is_valid_triangle?
-        if !(@triangle.kind_of? Array) || @triangle.empty?
-          return false
-        end
-    
-        return is_valid_triangle_row?
-    end
-
-    def is_valid_triangle_row?
-        (0...@triangle.length).each do |i|
-          row = @triangle[i]
-    
-          if !(row.kind_of? Array) && row.length == (i + 1)
-            return false
-          end
-
-          if !is_item_valid?(row)
-            return false
-          end
-
-        end
-        return true
-    end
-
-    def is_item_valid?(row)
-        row.each do |item|
-            return false unless item.kind_of? Integer
-        end
-        return true
-    end
-
+    return true
+  end
 end
--- test/hell_triangle_test.rb
+++ test/hell_triangle_test.rb
@@ -1,31 +1,31 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-require 'minitest/autorun'
-require 'hell_triangle'
+require "minitest/autorun"
+require "hell_triangle"
 
 class TriangleHellTest < Minitest::Test
-    def test_triangle_is_empty
-        triangle = HellTriangle.new([])
-        assert_nil triangle.total_sum
-    end
+  def test_triangle_is_empty
+    triangle = HellTriangle.new([])
+    assert_nil triangle.total_sum
+  end
 
-    def test_triangle_with_letter
-        triangle = HellTriangle.new([[1], [2, 'b']])
-        assert_nil triangle.total_sum
-    end
+  def test_triangle_with_letter
+    triangle = HellTriangle.new([[1], [2, "b"]])
+    assert_nil triangle.total_sum
+  end
 
-    def test_triangle_total_sum
-        triangle = HellTriangle.new([[1], [3, 7], [9, 7, 1]])
-        assert_equal 15, triangle.total_sum
-    end
+  def test_triangle_total_sum
+    triangle = HellTriangle.new([[1], [3, 7], [9, 7, 1]])
+    assert_equal 15, triangle.total_sum
+  end
 
-    def test_triangle_max_sum
-        triangle = HellTriangle.new([
-            [6],
-           [4, 5],
-          [9, 7, 1],
-         [9, 6, 8, 4]
-     ])
-     assert_equal 28, triangle.total_sum
-    end
+  def test_triangle_max_sum
+    triangle = HellTriangle.new([
+      [6],
+      [4, 5],
+      [9, 7, 1],
+      [9, 6, 8, 4],
+    ])
+    assert_equal 28, triangle.total_sum
+  end
 end
